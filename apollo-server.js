d0cff2b0c8e03a47e37061d165f402bec12c929e
Benny Powers <web@bennypowers.com>
2018-12-10 06:22:46 +0200

    Redis for persistence

--- apollo-server.js
+++ apollo-server.js
@@ -1,27 +1,30 @@
 import { readFileSync } from 'fs';
 import { ApolloServer, PubSub, gql } from 'apollo-server-express';
+import Redis from 'ioredis';
+import map from 'crocks/pointfree/map';
+
+const redis = new Redis(process.env.REDIS_URL);
 
 const pubsub = new PubSub();
 
 const MESSAGE_SENT = 'MESSAGE_SENT';
-
-const messages = [];
+const MESSAGES = 'messages';
 
 const typeDefs = gql(readFileSync(`${__dirname}/schema.graphql`, 'utf8'));
 
 const resolvers = {
   Mutation: {
-    sendMessage(root, { user, message }, context) {
+    async sendMessage(_, { user, message }) {
       const date = new Date().toISOString();
       const messageSent = { user, message, date };
-      messages.push({ user, message, date });
+      await redis.lpush(MESSAGES, JSON.stringify(messageSent));
       pubsub.publish(MESSAGE_SENT, { messageSent });
       return messageSent;
     },
   },
 
   Query: {
-    messages: () => messages,
+    messages: async () => await redis.lrange(MESSAGES, 0, -1).then(map(JSON.parse)),
   },
 
   Subscription: {
