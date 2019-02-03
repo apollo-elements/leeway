e3039b654222247264acd08f31ce17d6c68560a1
Benny Powers <web@bennypowers.com>
2019-02-03 11:33:13 +0200

    Fixes rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import workbox from 'rollup-plugin-workbox';
-import string from 'rollup-plugin-string';
 import graphql from 'rollup-plugin-graphql';
 import { terser } from 'rollup-plugin-terser';
 
