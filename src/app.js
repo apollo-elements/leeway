13388707bea946b83766e6a73aaafba74e4a00dd
Benny Powers <web@bennypowers.com>
2019-02-03 03:32:50 +0200

    Revert Previous

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 window.WebComponents.waitFor(async function resolveBody() {
-  const client = await (await import('./client').then(({ getClient }) => getClient()));
+  const client = await import('./client').then(({ getClient }) => getClient());
   window.__APOLLO_CLIENT__ = client;
   await Promise.all([
-    import('@apollo-elements/lit-apollo/apollo-query'),
-    import('@apollo-elements/lit-apollo/apollo-mutation'),
     import('./components/leeway-input-fields.js'),
     import('./components/leeway-messages.js'),
     import('./components/leeway-status-notifier.js'),
